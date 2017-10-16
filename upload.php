72963b0cf07c88f8be75710c6c0ef45fbad6fe9d
Pratik Parija <pp285@afsconnect1.njit.edu>
2017-10-16 00:00:37 -0400

    Cleaning up upload.php

--- upload.php
+++ upload.php
@@ -19,7 +19,6 @@ else{
 	if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
 		echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		$file = ($_FILES["fileToUpload"]["name"]);
-		//return $file;
 	}
 	else{
 		echo "Sorry, there was an error uploading your file.";
